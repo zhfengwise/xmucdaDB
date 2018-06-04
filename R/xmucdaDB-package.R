b3ea7eb6e9430b8bd7caa69495f6dd0a2170b5d9
linjinzhen <linjinzhen@hotmail.com>
2018-06-04 15:08:37 +0800

    添加包描述；升级版本号至0.5.0

---
+++ R/xmucdaDB-package.R
@@ -0,0 +1,24 @@
+#' xmucdaDB.
+#'
+#' @name xmucdaDB
+#' @docType package
+#'
+#' @title Data of "An Introduction to Categorical Data Analysis,
+#'   2nd edition by Alan Agresti"
+#'
+#' @description Data of "An Introduction to Categorical Data Analysis,
+#'   2nd edition by Alan Agresti".
+#'
+#'   Some of data files in this packages are from package "icda" by Brett Presnell
+#'   (http://users.stat.ufl.edu/~presnell/Courses/sta4504-2012sp/R-info.shtml).
+#'
+#' @details
+#' This package include data sets that may be used in the textbook.
+#' Using \code{data(package = "xmucdaDB")} to see all the data sets
+#' that are avaliable in this package.
+#'
+#' It also include two functions (\code{find_data_by_title} and
+#' \code{find_data_by_var}) to make it convenient to find required dataset
+#' from so many data sets in this package. See \code{\link{find_data_by_title}}.
+
+NULL
